02be2e2ddf2cd76851c7fb53cb30efd0ad3ccfec
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-11-06 13:05:07 +0530

    buyscar backend

---
+++ Middleware/Authentication.middleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const AuthenticationHandler = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(
+      token.split(" ")[1],
+      process.env.JWT_SECRET_KEY,
+      (err, decoded) => {
+        if (decoded) {
+          console.log(decoded);
+          req.body["dealerId"] = decoded.dealerId;
+          req.body["full_name"] = decoded.full_name;
+          next();
+        } else {
+          res.status(404).send({
+            message:
+              "Authorization failure.You are not authorized to perform this action"
+          });
+        }
+      }
+    );
+  } else {
+    res.status(404).send({ message: "Please Login first" });
+  }
+};
+
+module.exports = { AuthenticationHandler };
